1d2c3e1f8f5d54853fcf05e0fd87dd0ab631a99d
vitorialuz229 <vitorialuz831@gmail.com>
2024-07-24 18:41:14 -0300

    :lipstick: feat: Arrumando a identação do código

--- src/app/components/btn-primary/btn-primary.component.ts
+++ src/app/components/btn-primary/btn-primary.component.ts
@@ -1,23 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, Output, EventEmitter, input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-type BtnVariants = "primary" | "secondary";
+type BtnVariants = 'primary' | 'secondary';
 
 @Component({
   selector: 'btn-primary',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './btn-primary.component.html',
-  styleUrl: './btn-primary.component.scss'
+  styleUrl: './btn-primary.component.scss',
 })
 export class BtnPrimaryComponent {
-
-  @Input("btn-text") btnText: string = "";
+  @Input('btn-text') btnText: string = '';
   @Input() disabled: boolean = false;
   @Input() loading: boolean = false;
-  @Input() variant: BtnVariants = "primary";
+  @Input() variant: BtnVariants = 'primary';
 
-  @Output("submit") onSubmit = new EventEmitter();
+  @Output('submit') onSubmit = new EventEmitter();
 
   submit() {
     this.onSubmit.emit();
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
 import { NgOptimizedImage } from '@angular/common';
+
+import { HeaderComponent } from '../header/header.component';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent ],
+  imports: [
+    HeaderComponent,
+    NgOptimizedImage,
+    BtnPrimaryComponent,
+    NewsletterFormComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
